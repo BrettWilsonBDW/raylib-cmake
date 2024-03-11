960cca8fda1076c827e903145076a6d96d9a4393
TonicComa37 <brett.david.wilson.bdw@outlook.com>
2024-03-11 14:49:57 +0200

    updated example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,116 @@
 #include "raylib.h"
-#include "Debug.hpp"
+#include <cmath> // for sin and cos
+
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
+// used to get web width and height in browser
+#if defined(PLATFORM_WEB)
+EM_JS(int, getBrowserWidth, (), {
+    return window.innerWidth;
+});
+
+EM_JS(int, getBrowserHeight, (), {
+    return window.innerHeight;
+});
+#endif
+
+// class used to set up game loop
+class GameLoop
+{
+public:
+    GameLoop();
+    ~GameLoop();
+    void Run();
+
+private:
+    void Draw();
+    static void MainLoopHelper(void *userData); // needed for emscripten
+    int screenWidth{};
+    int screenHeight{};
+};
+
+GameLoop::GameLoop()
+{
+    screenWidth = 1920 / 2;
+    screenHeight = 1080 / 2;
+
+#if defined(PLATFORM_WEB)
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
+#else
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE); // set custom flags for desktop
+    SetTargetFPS(60);                                                            // do not set fps when used in browser for better web performance
+#endif
+
+    InitWindow(screenWidth, screenHeight, "Game name");
+    Run();
+}
+
+GameLoop::~GameLoop()
+{
+    CloseWindow();
+}
+
+void GameLoop::Draw()
+{
+    // set proper width and height in browser window
+#if defined(PLATFORM_WEB)
+    int padding = 30; // set padding to avoid scrollbar and browser edge overlap
+    SetWindowSize(getBrowserWidth() - padding, getBrowserHeight() - padding);
+#endif
+
+    BeginDrawing();
+    ClearBackground(DARKGRAY);
+
+    // rainbow triangle 
+    {
+        static unsigned char rainbowNum{0};
+
+        DrawTriangle(
+            Vector2{static_cast<float>(screenWidth / 2), static_cast<float>(screenHeight / 2 - 100)},
+            Vector2{static_cast<float>(screenWidth / 2 - 100), static_cast<float>(screenHeight / 2 + 100)},
+            Vector2{static_cast<float>(screenWidth / 2 + 100), static_cast<float>(screenHeight / 2 + 100)},
+            Color{
+                static_cast<unsigned char>((1 + sin(0.3 * rainbowNum)) * 127.5),
+                static_cast<unsigned char>((1 + sin(0.3 * rainbowNum + 2)) * 127.5),
+                static_cast<unsigned char>((1 + sin(0.3 * rainbowNum + 4)) * 127.5),
+                255});
+
+        rainbowNum++;
+        if (rainbowNum > 256)
+        {
+            rainbowNum = 0;
+        }
+    }
+
+    EndDrawing();
+}
+
+// method used to call all game logic
+void GameLoop::MainLoopHelper(void *userData)
+{
+    GameLoop *gameLoop = static_cast<GameLoop *>(userData);
+
+    gameLoop->Draw(); // add more game logic methods here eg: Update();
+}
+
+// used to run game loop logic
+void GameLoop::Run()
+{
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop_arg(MainLoopHelper, this, 0, 1);
+#else
+    while (!WindowShouldClose())
+    {
+        MainLoopHelper(this);
+    }
+#endif
+}
 
 int main()
 {
-    // rgb and transparency
-    Color black = Color{0, 0, 0, 255};
+    GameLoop gameLoop; // create game loop object
 
-    int windowWidth{1920 / 2};
-    int windowHeight{1080 / 2};
-
-    InitWindow(windowWidth, windowHeight, "Game name");
-    // resize window
-    windowWidth = GetScreenWidth();
-    windowHeight = GetScreenHeight();
-    SetWindowSize(windowWidth, windowHeight);
-    // SetWindowPosition(1920, 10);
-
-    SetExitKey(KEY_NULL);
-
-    //init the debug class
-    Debug debug;
-
-    SetTargetFPS(60);
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(black);
-
-        // debug  menu
-        if (debug.ToggleState())
-        {
-            int fps = GetFPS();
-
-            // call drawItems once and passItems before as many times as needed
-
-            debug.PassItems("Debug Menu:\n");
-            debug.PassItems("FPS: %d\n", fps);
-            debug.DrawItems(3, 3, 16);
-        }
-
-        EndDrawing();
-    }
-
-    CloseWindow();
     return 0;
 }
