e2bf4ebd886985dba82163319689d0e213d80cf7
Brett Wilson <brett.david.wilson.message@outlook.com>
2024-04-21 14:24:33 +0200

    Update build_for_web.bat

--- tools/build_for_web.bat
+++ tools/build_for_web.bat
@@ -26,6 +26,11 @@ REM Change the current directory to 'build-web'
 IF "%1"=="async" (
     echo using asyncify
     call emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html" -DASYNCIFY=1
+) ELSE IF "%1"=="debug" (
+    call emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"
+) ELSE IF NOT "%1"=="" (
+    echo Unknown argument: %1
+    exit /b 1
 ) ELSE (
     call emcmake cmake .. -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS="-s USE_GLFW=3" -DCMAKE_EXECUTABLE_SUFFIX=".html"
 )
